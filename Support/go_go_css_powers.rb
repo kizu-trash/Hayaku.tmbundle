4e47900ade56ebf856421fa8eb043cacdaf10b35
kizu <kizu@macbook-kizu.local>
2010-09-14 02:21:48 +0400

    Moved variables to a variables, aw

--- Support/go_go_css_powers.rb
+++ Support/go_go_css_powers.rb
@@ -6,18 +6,7 @@ require_support = ENV['TM_BUNDLE_SUPPORT'] + '/' unless ENV['TM_BUNDLE_SUPPORT']
 
 require require_support + 'ololo_dictionary.rb'
 require require_support + 'parsing_parser.rb'
-
-$syntax_space = ENV['TM_CSS_SPACE'] || ''
-$syntax_tab = ''
-if ENV['TM_SOFT_TABS'] == 'YES'
-  ENV['TM_TAB_SIZE'].to_i.times { $syntax_tab<<' ' }
-else
-  $syntax_tab = "\t"
-end
-$extra_indent = (ENV['TM_CURRENT_LINE'].match(/\s+/) || [''])[0].sub($syntax_tab,'')
-
-$indent = $extra_indent + $syntax_tab
-$before_closing = $extra_indent + (ENV['TM_CSS_BEFORE_CLOSING'] || '');
+require require_support + 'variable_variables.rb'
 
 # ahaha lol method!11
 def ExpandCSSAbbreviation( inputs )
