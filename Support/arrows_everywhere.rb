093162777c6b3a8511457517584842a3ef1ce64d
kizu <kizmarh@gmail.com>
2011-01-14 02:55:41 +0300

    Added commands for nice changing of numeric values

---
+++ Support/arrows_everywhere.rb
@@ -0,0 +1,33 @@
+#*_*#
+
+# for testing look at current support folder
+require_support = ''
+require_support = ENV['TM_BUNDLE_SUPPORT'] + '/' unless ENV['TM_BUNDLE_SUPPORT'].include? 'Ruby.tmbundle'
+
+require require_support + 'ololo_dictionary.rb'
+
+if ENV['TM_SELECTED_TEXT']
+  if ENV['TM_SELECTED_TEXT'].index(/^[\d\-\.]+$/)
+    print "${0:"+ (ENV['TM_SELECTED_TEXT'].to_f + Modifier).to_s.gsub(/\.0$/,'') +"}"
+  else
+    print "${0:"+ ENV['TM_SELECTED_TEXT'] +"}"
+  end
+else
+  # get left and right part from caret position
+  left = ''
+  left = ENV['TM_CURRENT_LINE'].slice(0..ENV['TM_LINE_INDEX'].to_i-1) if ENV['TM_LINE_INDEX'].to_i > 0
+  right = ENV['TM_CURRENT_LINE'].slice(ENV['TM_LINE_INDEX'].to_i..-1)
+
+  if ENV['TM_CURRENT_WORD'].index(/\d/)
+    # mark caret position for digit find
+    result = left + "§" + right + '${0}'
+    print result.gsub(/([\w\-\.]*)§([\w\-\.]*)/){
+      ($1 + $2).gsub(/([\d\-\.]+)/){
+        ($1.to_f + Modifier).to_s.gsub(/\.0$/,'') + '${1}'
+      }
+    }
+  else
+    # keep caret position and add extra lulz
+    print left + '${1}' + right + '${0}'
+  end
+end
