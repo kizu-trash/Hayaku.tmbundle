97a2955207e6101d9703a07f2a5a4a6444e140c7
kizu <kizmarh@gmail.com>
2011-01-14 21:46:29 +0300

    Importance toggler added

---
+++ Support/!!!111.rb
@@ -0,0 +1,21 @@
+if ENV['TM_SELECTED_TEXT']
+  # do nothing
+  print "${0:"+ ENV['TM_SELECTED_TEXT'] +"}"
+else
+  # get left and right part from caret position
+  left = ''
+  left = ENV['TM_CURRENT_LINE'].slice(0..ENV['TM_LINE_INDEX'].to_i-1) if ENV['TM_LINE_INDEX'].to_i > 0
+  right = ENV['TM_CURRENT_LINE'].slice(ENV['TM_LINE_INDEX'].to_i..-1)
+
+  print (left + "§" + right + '${0}').gsub(/([\{\;]?[^\;\}]*)§([^\;\}]*[\}\;]?)/){
+    if ($1 + $2).index('!important')
+      if ($2).index(' !important')
+        ($1 + "§" + $2).sub('§','${1}').gsub(' !important','')
+      else
+        ($1 + $2).gsub(/ ?!important/,'${1}')
+      end
+    else
+      ($1 + "§" + $2).sub('§','${1}').sub(/\s*;?(\s*)$/,' !important;\1')
+    end
+  }
+end
