7d5e85565bba32c7f3f902b91792318707a71b46
kizu <kizmarh@gmail.com>
2011-02-27 00:32:30 +0300

    Added Find and Replace Selected or Word action

--- Support/_HayakuLib.rb
+++ Support/_HayakuLib.rb
@@ -5,6 +5,16 @@ require ENV['TM_SUPPORT_PATH'] + '/lib/escape.rb'
 # Partially inspired by Duane Johnson's Multiple Arbitrary Simultaneous Carets http://inquirylabs.com/blog2009/my-textmate-bundle/
 # But with a lot of improvements, ideas, quirks and things. And made from scratch, yeah.
 
+def FindSimilliar()
+  result = e_sn(STDIN.read)
+  
+  @original = result.match(/⦉([^⦊]*)⦊/)[0].gsub(/[⦉⦊]/,'')
+  
+  result.gsub!(/(#{Regexp.escape(@original)})(?!⦊)/,'⟨\1⟩')
+  
+  ReplaceCarets(result)
+end
+
 def SetCaret()
   if ENV['TM_SELECTED_TEXT']
     # need to toggle current caret and replace it with initial tabstop (or it can't be so? Or do not enter replace mode after toggling?)
@@ -28,11 +38,17 @@ def SetCaret()
   end
 end
 
-def ReplaceCarets()
-  result = e_sn(STDIN.read)
+def ReplaceCarets(someInput)
+  result = someInput || e_sn(STDIN.read)
   result.sub!(/⟨([^⟩]*)⟩/m,'⦉\1⦊') if !result.index(/⦉[^⦊]*⦊/)
-  result.gsub!(/⟨([^⟩]*)⟩/m,'${1/^(⟨)?.+$/(?1:⟨\1⟩:$0)/gm}') if result.index(/⟨[^⟩]*⟩/)
-  result.gsub!(/⦉([^⦊]*)⦊/m,'${1:⟨\1⟩}$0${1/^.+$//gm}') if result.index(/⦉[^⦊]*⦊/)
+  
+  if someInput
+    result.gsub!(/⟨([^⟩]*)⟩/m,'${1}') if result.index(/⟨[^⟩]*⟩/)
+    result.gsub!(/⦉([^⦊]*)⦊/m,'${1:\1}$0${1/^.+$//gm}') if result.index(/⦉[^⦊]*⦊/)
+  else
+    result.gsub!(/⟨([^⟩]*)⟩/m,'${1/^(⟨)?.+$/(?1:⟨\1⟩:$0)/gm}') if result.index(/⟨[^⟩]*⟩/)
+    result.gsub!(/⦉([^⦊]*)⦊/m,'${1:⟨\1⟩}$0${1/^.+$//gm}') if result.index(/⦉[^⦊]*⦊/)
+  end
 
   print result
 end
