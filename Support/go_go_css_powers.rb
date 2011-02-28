21415fde5c16d3a3419bfdd251e32c4c91b29b5d
kizu <kizmarh@gmail.com>
2011-03-01 00:33:00 +0300

    Some stupid stupids

--- Support/go_go_css_powers.rb
+++ Support/go_go_css_powers.rb
@@ -5,9 +5,16 @@ require_support = ''
 require_support = ENV['TM_BUNDLE_SUPPORT'] + '/' unless ENV['TM_BUNDLE_SUPPORT'].include? 'Ruby.tmbundle'
 
 require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes.rb'
+require ENV['TM_SUPPORT_PATH'] + '/lib/escape.rb'
 require require_support + 'ololo_dictionary.rb'
 require require_support + 'parsing_parser.rb'
 require require_support + 'variable_variables.rb'
+
+@input = e_sn(STDIN.read)
+
+def WTF(wut)
+  TextMate.exit_show_tool_tip 'WTF: ' + wut
+end
 
 # check if there are prefixes for a result - move to parser
 # TODO: last \n and indent must be obly if it's the last thing in abbreviation, so need to move it to outside someday
@@ -26,6 +33,40 @@ end
 
 # ahaha lol method!11
 def ExpandCSSAbbreviation( inputs )
+  @results = []
+  
+  # Split input by properties delimiter (TODO: must understand newline for SASS etc.)
+  inputs.split(/([^:;]*\s+)?(\w+(?:\:[^;]*)?);?/).each do |input|
+    @result = ''
+    
+    if input.strip.length
+      # Find if there is delimiter for value
+      @startSpace = input.match(/^\s*/)[0]
+      @dirtySplit = input.strip.split(/:\s*/)
+    
+      @property = ''
+      # if there is already fine name do nothing
+      if Props.select{ |item| item['name'] == @dirtySplit[0]}[0]
+        @result = input + '; /* doing nothing */'
+      else
+        @result += @startSpace + @dirtySplit[0] if @dirtySplit[0]
+        @result += ':' + $syntax_space + @dirtySplit[1] if @dirtySplit[1]
+        @result += ';'
+
+        # Too much of :, need smth to do with it
+        WTF('Too much ::::') if @dirtySplit.length > 2 
+      end
+    end    
+    if @result and @result != ''
+      @results << @result
+    else
+      @results << 'ahaha'
+    end
+  end
+  return @results.join
+end
+
+def ExpandCSSAbbreviationOld( inputs )
   # another thing to move to config - inputs delimiter
   results = []
   any_ok = false
@@ -70,11 +111,10 @@ def ExpandCSSAbbreviation( inputs )
   
 end
 
-result = ExpandCSSAbbreviation(ENV['TM_CURRENT_LINE'].strip)
+result = ExpandCSSAbbreviation(@input)
 
-if result && result != '' && !ENV['TM_CURRENT_LINE'].match(/(;|\*\/)\s*$/)
+if result && result != ''
   print result
 else
-  TextMate.exit_discard if ENV['TM_SELECTED_TEXT']
-  TextMate.exit_insert_text ' '
+  TextMate.exit_discard
 end
