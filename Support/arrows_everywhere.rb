6387e1091ef757f38c58fef84c85edae5d92eb06
kizu <kizmarh@gmail.com>
2011-07-17 22:28:30 +0400

    * Added expand and swapping to sinlge-quoted html style attribute. * Basic “Edit property” placeholder.

--- Support/arrows_everywhere.rb
+++ Support/arrows_everywhere.rb
@@ -9,20 +9,24 @@ require ENV['TM_SUPPORT_PATH'] + '/lib/exit_codes.rb'
 require ENV['TM_SUPPORT_PATH'] + '/lib/escape.rb'
 
 def swapRule(property,value)
-  foundValues = valuesOf(property)
-  # must be no case-sensitive
-  currentIndex = foundValues.index(value) if foundValues
-  if currentIndex
-    if Modifier < 0 
-      newvalue = foundValues[currentIndex+1]
-      newvalue = foundValues[0] if currentIndex == foundValues.length-1
-    else
-      newvalue = foundValues[currentIndex-1]
-      newvalue = foundValues[-1] if currentIndex == 0
+  if Modifier != 0
+    foundValues = valuesOf(property)
+    # must be no case-sensitive
+    currentIndex = foundValues.index(value) if foundValues
+    if currentIndex
+      if Modifier < 0 
+        newvalue = foundValues[currentIndex+1]
+        newvalue = foundValues[0] if currentIndex == foundValues.length-1
+      else
+        newvalue = foundValues[currentIndex-1]
+        newvalue = foundValues[-1] if currentIndex == 0
+      end
     end
+    # if there is a partial writing autocomplete it
+    newvalue = foundValues[0] if !currentIndex and foundValues
+  else
+    newvalue = '${1:'+value+'}'
   end
-  # if there is a partial writing autocomplete it
-  newvalue = foundValues[0] if !currentIndex and foundValues
   return newvalue if newvalue
   TextMate.exit_discard
 end
@@ -36,22 +40,25 @@ def findRule()
   result = left + "‸" + right
 
   # If caret is on property
-  result.gsub(/^(.*?)([a-z\-]*)‸([a-z\-]*)(:\s*)([^;]*)([;}].*)$/m){
-    result = $1+$2+'${1}'+$3+$4+swapRule($2+$3,$5)+$6
+  result.gsub!(/^(.*?)([a-z\-]*)‸([a-z\-]*)(:\s*)([^;]*)([;}].*)$/m){
+    $1+$2+'${1}'+$3+$4+swapRule($2+$3,$5)+$6
   }
 
   # If caret is on value
-  result.gsub(/^(.*?)([a-z\-]*)(:\s*)([^;]*)‸([^;]*)([;}].*)$/m){
+  result.gsub!(/^(.*?)([a-z\-]*)(:\s*)([^;]*)‸([^;]*)([;}].*)$/m){
     rule = swapRule($2,$4+$5)
     whereTo = [$4.length,rule.length].min
     whereTo = rule.length if $5.length == 0
-    result = $1+$2+$3+rule.insert(whereTo,'${1}')+$6
+    $1+$2+$3+rule.insert(whereTo,'${1}')+$6
   }
 
   # If caret is after 
-  result.gsub(/^(.*?)([a-z\-]*)(:\s*)([^;]*)([;}].*)‸([^:]*)$/m){
-    result = $1+$2+$3+swapRule($2,$4)+$5+'${1}'+$6
+  result.gsub!(/^(.*?)([a-z\-]*)(:\s*)([^;]*)([;}].*)‸([^:]*)$/m){
+    $1+$2+$3+swapRule($2,$4)+$5+'${1}'+$6
   }
+
+  # Remove ordinal tabstop if editing
+  result.gsub!('${1}','') if Modifier == 0
 
   # need escaping, whitespace at the ^ and better tabstops in value
   return result if !result.include?('‸')
@@ -69,7 +76,7 @@ else
   left = ENV['TM_CURRENT_LINE'].slice(0..ENV['TM_LINE_INDEX'].to_i-1) if ENV['TM_LINE_INDEX'].to_i > 0
   right = ENV['TM_CURRENT_LINE'].slice(ENV['TM_LINE_INDEX'].to_i..-1)
 
-  if ENV['TM_CURRENT_WORD'].index(/\d/)
+  if ENV['TM_CURRENT_WORD'].index(/\d/) and Modifier != 0
     # mark caret position for digit find
     result = left + "‸" + right
     if result.match(/(^|(?!\d)\w)‸((?:(?!\d)\w)*)(-?\d*\.?\d+|\d+(?:\.\d+)?)/)
@@ -100,7 +107,7 @@ else
     print e_sn(result).gsub('⦉\${0}⦊','${0}')
     
   else
-    swapping = findRule() if ENV['TM_SCOPE'].include?('source.css') or ENV['TM_SCOPE'].include?('string.quoted.double.html')
+    swapping = findRule() if ENV['TM_SCOPE'].include?('source.css') or ENV['TM_SCOPE'].include?('string.quoted.double.html') or ENV['TM_SCOPE'].include?('string.quoted.single.html')
     print swapping if swapping
     TextMate.exit_discard if !swapping
   end
